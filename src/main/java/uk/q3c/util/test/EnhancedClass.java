347ca942b9513f807f93704f65c92359a8ff17b3
David Sowerby <david.sowerby@virgin.net>
2017-08-07 10:33:32 +0100

    Test added for DefaultClassNameUtils.  New test package in src/main with test helpers

---
+++ src/main/java/uk/q3c/util/test/EnhancedClass.java
@@ -0,0 +1,12 @@
+package uk.q3c.util.test;
+
+/**
+ * Created by David Sowerby on 07 Aug 2017
+ */
+public class EnhancedClass {
+
+    @NotOnWeekends
+    public void justForAOPAnnotation() {
+
+    }
+}
